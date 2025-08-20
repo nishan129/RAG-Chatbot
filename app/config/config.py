8e5a2baa383772362d796c347dccb57550530373
nishant129 <nishantborkar139@gmail.com>
2025-08-20 22:03:02 +0530

    change chunk size and docker code

--- app/config/config.py
+++ app/config/config.py
@@ -5,8 +5,8 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 DB_FAISS_PATH = "vectorestore/db_faiss"
 DATA_PATH = "data2/"
-CHUNK_SIZE = 500
-CHUNK_OVERLAP = 100
+CHUNK_SIZE = 10000
+CHUNK_OVERLAP = 1000
 
 DATABASE_NAME = "rag_db"
 COLLECTION_NAME = "rag_collection"
